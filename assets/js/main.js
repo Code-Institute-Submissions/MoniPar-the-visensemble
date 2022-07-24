fbc02619403a7736d545104682c2ab6b9aa8c066
Monique Parnis <monique.parnis@gmail.com>
2022-07-24 16:06:36 +0000

    Add and Style About Hero, include script for read more button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
+// Navigation Hamburger Button //
+
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
 });
+
+// Read More Read Less Button //
+
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show--more");
+  if (readMoreBtn.innerText === "READ MORE") {
+    readMoreBtn.innerText = "READ LESS";
+  } else {
+    readMoreBtn.innerText = "READ MORE";  
+  }
+})
