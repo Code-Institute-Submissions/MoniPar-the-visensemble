342d9843cc9a8d77324f7ffa3cea54ec24d22904
Monique Parnis <monique.parnis@gmail.com>
2022-07-25 23:23:35 +0000

    Add and Style Skip to Main button and Back to Top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,16 @@ readMoreBtn.addEventListener("click", (e) => {
   } else {
     readMoreBtn.innerText = "READ MORE";  
   }
-})
+});
+
+// Back To Top Button //
+
+const toTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
